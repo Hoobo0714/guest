a9416957eb2cbfd1b467d0fabfcf23bc69616c61
Hoobo0714 <Hoobo0714@foxmail.com>
2019-01-21 13:29:02 +0800

    add templates

--- sign/views.py
+++ sign/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello django")
+    return render(request,'index.html')
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        if username == 'admin' and password == '123':
+            return HttpResponse('login success')
+        else:
+            return render(request,'index.html',{'error':'username or password error!!'})
