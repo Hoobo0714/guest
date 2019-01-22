a127c16f5dd863357dd0065a2c13d65ed137fc90
Hoobo0714 <Hoobo0714@foxmail.com>
2019-01-22 09:29:12 +0800

    add session

--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
 # Create your views here.
 
 def index(request):
@@ -9,10 +10,17 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username','')
         password = request.POST.get('password','')
+        user = auth
         if username == 'admin' and password == '123':
-            return HttpResponseRedirect('/event_manage/')
+            response = HttpResponseRedirect('/event_manage/')
+            #response.set_cookie('user',username,3600)
+            request.session['user'] = username
+            return response
+
         else:
             return render(request,'index.html',{'error':'username or password error!!'})
 
 def event_manage(request):
-    return render(request,'event_manage.html')
+    #username = request.COOKIES.get('user','')
+    username = request.session.get('user','')
+    return render(request,'event_manage.html',{'user':username})
